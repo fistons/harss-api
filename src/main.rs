7e4ee1633ed0f699fe65d60ff0da69b06a7d7775
Eric Fistons <eric@pedr0.net>
2021-03-03 22:32:09 +0100

    It works a bit

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,34 @@ use actix_web::http::StatusCode;
 use diesel::r2d2::ConnectionManager;
 use diesel::SqliteConnection;
 
-use diesel::prelude::*; 
-
+use crate::model::Channel;
 
 mod schema;
 mod model;
 
 type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    "Hi!"
+#[get("/channel/{id}")]
+async fn get_channel(id: web::Path<i32>, db: web::Data<DbPool>) -> web::Json<Channel> {
+    let connection = db.get().unwrap();
+    web::Json(web::block(move || model::db::select_by_id(id.into_inner(), &connection)).await.unwrap())
 }
 
-#[post("/channel")]
+#[get("/channels")]
+async fn get_channels(db: web::Data<DbPool>) -> web::Json<Vec<Channel>> {
+    let connection = db.get().unwrap();
+    web::Json(web::block(move || model::db::select_all(&connection)).await.unwrap())
+}
+
+
+#[post("/channels")]
 async fn new_channel(new_channel: web::Json<model::NewChannel>, db: web::Data<DbPool>) -> HttpResponse {
     println!("Recording new channel {:?}", new_channel);
-    
+
     let connection = db.get().unwrap();
     let data = new_channel.into_inner();
 
-    web::block(move || {
-        use crate::schema::channels::dsl::*;
-
-        
-        diesel::insert_into(channels).values(&data).execute(connection).unwrap();
-        
-        Ok(())
-    });
+    web::block(move || model::db::insert(data, &connection)).await.unwrap();
 
     HttpResponse::new(StatusCode::ACCEPTED)
 }
@@ -42,8 +42,8 @@ async fn new_channel(new_channel: web::Json<model::NewChannel>, db: web::Data<Db
 async fn main() -> std::io::Result<()> {
 
     // set up database connection pool
-    let connspec = "./test.db";
-    let manager = ConnectionManager::<SqliteConnection>::new(connspec);
+    let connection_spec = "./test.db";
+    let manager = ConnectionManager::<SqliteConnection>::new(connection_spec);
     let pool: DbPool = r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.");
@@ -53,7 +53,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .data(pool.clone())
-            .service(hello)
+            .service(get_channels)
+            .service(get_channel)
             .service(new_channel)
     })
         .bind("127.0.0.1:8080")?
--- src/model.rs
+++ src/model.rs
@@ -1,20 +1,52 @@
-/// # A bunch of stuff
+use diesel::SqliteConnection;
 use serde::{Deserialize, Serialize};
 use crate::schema::channels;
 
-#[table_name="channels"]
+
 #[derive(Debug, Serialize, Deserialize, Clone, Insertable)]
+#[table_name = "channels"]
 pub struct NewChannel {
     pub name: String,
-    pub url: String
+    pub url: String,
 }
 
 /// Source of articles, over da web
-#[table_name="channels"]
 #[derive(Debug, Serialize, Deserialize, Clone, Queryable)]
 pub struct Channel {
     pub id: i32,
     pub name: String,
-    pub url: String
+    pub url: String,
 }
 
+pub mod db {
+    use crate::schema::channels::dsl::*;
+    use crate::schema::channels;
+    use diesel::prelude::*;
+
+    use diesel::SqliteConnection;
+    
+    use super::Channel as Channel;
+    use super::NewChannel as NewChannel;
+    
+
+    pub fn insert(new_channel: NewChannel, db: &SqliteConnection) -> Result<(), diesel::result::Error> {
+    
+
+        diesel::insert_into(channels).values(&new_channel).execute(db)?;
+        Ok(())
+    }
+
+    pub fn select_all(db: &SqliteConnection) -> Result<Vec<Channel>, diesel::result::Error> {
+        
+
+        let r = channels.load::<Channel>(db)?;
+        Ok(r)
+    }
+
+    pub fn select_by_id(predicate: i32, db: &SqliteConnection) -> Result<Channel, diesel::result::Error> {
+        
+
+        Ok(channels.filter(id.eq(predicate))
+            .first::<Channel>(db)?)
+    }
+}
