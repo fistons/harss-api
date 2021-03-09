16e7ea481db92c15c0f37f8c094c844a22f0f186
Eric Fistons <eric@pedr0.net>
2021-03-09 22:51:17 +0100

    Models for items

--- src/model.rs
+++ src/model.rs
@@ -1,52 +1,70 @@
-use diesel::SqliteConnection;
-use serde::{Deserialize, Serialize};
-use crate::schema::channels;
+pub mod channel {
+    use serde::{Deserialize, Serialize};
 
-
-#[derive(Debug, Serialize, Deserialize, Clone, Insertable)]
-#[table_name = "channels"]
-pub struct NewChannel {
-    pub name: String,
-    pub url: String,
-}
-
-/// Source of articles, over da web
-#[derive(Debug, Serialize, Deserialize, Clone, Queryable)]
-pub struct Channel {
-    pub id: i32,
-    pub name: String,
-    pub url: String,
-}
-
-pub mod db {
-    use crate::schema::channels::dsl::*;
     use crate::schema::channels;
-    use diesel::prelude::*;
 
-    use diesel::SqliteConnection;
-    
-    use super::Channel as Channel;
-    use super::NewChannel as NewChannel;
-    
-
-    pub fn insert(new_channel: NewChannel, db: &SqliteConnection) -> Result<(), diesel::result::Error> {
-    
-
-        diesel::insert_into(channels).values(&new_channel).execute(db)?;
-        Ok(())
+    #[derive(Debug, Serialize, Deserialize, Clone, Insertable)]
+    #[table_name = "channels"]
+    pub struct NewChannel {
+        pub name: String,
+        pub url: String,
     }
 
-    pub fn select_all(db: &SqliteConnection) -> Result<Vec<Channel>, diesel::result::Error> {
-        
-
-        let r = channels.load::<Channel>(db)?;
-        Ok(r)
+    /// Source of articles, over da web
+    #[derive(Debug, Serialize, Deserialize, Clone, Queryable)]
+    pub struct Channel {
+        pub id: i32,
+        pub name: String,
+        pub url: String,
     }
 
-    pub fn select_by_id(predicate: i32, db: &SqliteConnection) -> Result<Channel, diesel::result::Error> {
-        
+    pub mod db {
+        use diesel::prelude::*;
+        use diesel::SqliteConnection;
 
-        Ok(channels.filter(id.eq(predicate))
-            .first::<Channel>(db)?)
+        use crate::schema::channels::dsl::*;
+
+        use super::Channel;
+        use super::NewChannel;
+
+        pub fn insert(new_channel: NewChannel, db: &SqliteConnection) -> Result<(), diesel::result::Error> {
+            diesel::insert_into(channels).values(&new_channel).execute(db)?;
+            Ok(())
+        }
+
+        pub fn select_all(db: &SqliteConnection) -> Result<Vec<Channel>, diesel::result::Error> {
+            let r = channels.load::<Channel>(db)?;
+            Ok(r)
+        }
+
+        pub fn select_by_id(predicate: i32, db: &SqliteConnection) -> Result<Channel, diesel::result::Error> {
+            Ok(channels.filter(id.eq(predicate))
+                .first::<Channel>(db)?)
+        }
     }
 }
+
+pub mod items {
+    use serde::{Deserialize, Serialize};
+
+    use crate::schema::items;
+
+    #[derive(Debug, Serialize, Deserialize, Clone, Insertable)]
+    #[table_name = "items"]
+    pub struct NewItem {
+        pub title: String,
+        pub url: String,
+        pub content: String,
+        pub channel_id: i32,
+    }
+
+    #[derive(Debug, Serialize, Deserialize, Clone, Queryable)]
+    pub struct Item {
+        pub id: i32,
+        pub title: String,
+        pub url: String,
+        pub content: String,
+        pub channel_id: i32,
+    }
+}
+
