aa4c142cfbdb9e11b24b8d247d4d55aafb8dae9a
Eric Fistons <eric@pedr0.net>
2021-03-27 21:08:20 +0100

    Listen everywhere

--- src/main.rs
+++ src/main.rs
@@ -139,7 +139,7 @@ async fn main() -> std::io::Result<()> {
             .service(get_items)
             .service(fs::Files::new("/", "./static/").index_file("index.html"))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
