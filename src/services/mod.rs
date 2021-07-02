8252f9e154f1c4d3b8a3159d638e0fb322929390
Eric <eric@pedr0.net>
2021-07-02 23:20:50 +0200

    🧹 Cleanup

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -24,12 +24,7 @@ impl GlobalService {
         }
     }
 
-    pub fn refresh(
-        &self,
-        // item_service: &ItemService,
-        // channel_service: &ChannelService,
-        user_id: i32,
-    ) -> Result<(), diesel::result::Error> {
+    pub fn refresh(&self, user_id: i32) -> Result<(), diesel::result::Error> {
         let channels = self.channel_service.select_all_by_user_id(user_id)?;
 
         for channel in channels.iter() {
@@ -38,13 +33,7 @@ impl GlobalService {
         Ok(())
     }
 
-    pub fn refresh_chan(
-        &self,
-        // item_service: &ItemService,
-        // channel_service: &ChannelService,
-        channel_id: i32,
-        user_id: i32,
-    ) -> Result<(), diesel::result::Error> {
+    pub fn refresh_chan(&self, channel_id: i32, user_id: i32) -> Result<(), diesel::result::Error> {
         let channel = self
             .channel_service
             .select_by_id_and_user_id(channel_id, user_id)?;
