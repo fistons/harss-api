2ce3fccbb0465c2a2f0f8a89005837bc31576d18
Eric <eric@pedr0.net>
2022-03-13 14:44:34 +0100

    ✨ Implements Page/Pagination model and new doc UI

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 use serde::{Deserialize, Serialize};
 
 use entity::sea_orm_active_enums::UserRole;
@@ -83,4 +85,49 @@ impl HttpNewItem {
             channel_id,
         }
     }
+}
+
+/// # Paging parameters
+#[derive(Debug, Deserialize)]
+pub struct PageParameters {
+    /// * The `page` field should be superior or equals to 1
+    page: Option<usize>,
+    /// * The `size` field should be between 1 and 20
+    size: Option<usize>,
+}
+
+impl PageParameters {
+    /// Return the given page, or 1 if not provided.
+    /// If the given page is 0, return 1.
+    pub fn get_page(&self) -> usize {
+        self.page.unwrap_or(1).max(1)
+    }
+
+    /// Return the given size of the page, or 20 if not provided.
+    /// The result is clamped between 1 and 200.
+    pub fn get_size(&self) -> usize {
+        self.size.unwrap_or(20).clamp(1, 200)
+    }
+}
+
+/// # Paged result
+///
+/// Return the elements matching the request in page, alongside the page context.
+#[derive(Debug, Serialize)]
+pub struct PagedResult<T>
+where
+    T: Serialize + Debug,
+{
+    /// Actual content.
+    pub content: Vec<T>,
+    /// Number of the page.
+    pub page: usize,
+    /// Desired size of the page.
+    pub page_size: usize,
+    /// Total number of pages.
+    pub total_pages: usize,
+    /// Number of elements returned.
+    pub elements_number: usize,
+    /// Total number of elements.
+    pub total_items: usize,
 }
