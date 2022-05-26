ff1b21efa49e375ddc8f52e754b695e45009c216
Eric <eric@pedr0.net>
2022-05-26 10:24:45 +0200

    🔧 Entity with expanded format

--- entity/src/users_items.rs
+++ entity/src/users_items.rs
@@ -3,45 +3,84 @@
 use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
-#[sea_orm(table_name = "users_items")]
+#[derive(Copy, Clone, Default, Debug, DeriveEntity)]
+pub struct Entity;
+
+impl EntityName for Entity {
+    fn table_name(&self) -> &str {
+        "users_items"
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, DeriveModel, DeriveActiveModel, Serialize, Deserialize)]
 pub struct Model {
-    #[sea_orm(primary_key, auto_increment = false)]
     pub user_id: i32,
-    #[sea_orm(primary_key, auto_increment = false)]
     pub item_id: i32,
-    #[sea_orm(primary_key, auto_increment = false)]
     pub channel_id: i32,
     pub read: bool,
     pub starred: bool,
 }
 
-#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+#[derive(Copy, Clone, Debug, EnumIter, DeriveColumn)]
+pub enum Column {
+    UserId,
+    ItemId,
+    ChannelId,
+    Read,
+    Starred,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DerivePrimaryKey)]
+pub enum PrimaryKey {
+    UserId,
+    ItemId,
+    ChannelId,
+}
+
+impl PrimaryKeyTrait for PrimaryKey {
+    type ValueType = (i32, i32, i32);
+    fn auto_increment() -> bool {
+        false
+    }
+}
+
+#[derive(Copy, Clone, Debug, EnumIter)]
 pub enum Relation {
-    #[sea_orm(
-        belongs_to = "super::channels::Entity",
-        from = "Column::ChannelId",
-        to = "super::channels::Column::Id",
-        on_update = "Cascade",
-        on_delete = "Cascade"
-    )]
     Channels,
-    #[sea_orm(
-        belongs_to = "super::items::Entity",
-        from = "Column::ItemId",
-        to = "super::items::Column::Id",
-        on_update = "Cascade",
-        on_delete = "Cascade"
-    )]
     Items,
-    #[sea_orm(
-        belongs_to = "super::users::Entity",
-        from = "Column::UserId",
-        to = "super::users::Column::Id",
-        on_update = "Cascade",
-        on_delete = "Cascade"
-    )]
     Users,
+}
+
+impl ColumnTrait for Column {
+    type EntityName = Entity;
+    fn def(&self) -> ColumnDef {
+        match self {
+            Self::UserId => ColumnType::Integer.def(),
+            Self::ItemId => ColumnType::Integer.def(),
+            Self::ChannelId => ColumnType::Integer.def(),
+            Self::Read => ColumnType::Boolean.def(),
+            Self::Starred => ColumnType::Boolean.def(),
+        }
+    }
+}
+
+impl RelationTrait for Relation {
+    fn def(&self) -> RelationDef {
+        match self {
+            Self::Channels => Entity::belongs_to(super::channels::Entity)
+                .from(Column::ChannelId)
+                .to(super::channels::Column::Id)
+                .into(),
+            Self::Items => Entity::belongs_to(super::items::Entity)
+                .from(Column::ItemId)
+                .to(super::items::Column::Id)
+                .into(),
+            Self::Users => Entity::belongs_to(super::users::Entity)
+                .from(Column::UserId)
+                .to(super::users::Column::Id)
+                .into(),
+        }
+    }
 }
 
 impl Related<super::channels::Entity> for Entity {
