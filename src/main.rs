54d0e37cc9142cbc82a9e87eb6bcdfc9d060792c
Eric <emercier@weborama.com>
2022-07-14 16:06:27 -0400

    🏗 Externalize poller.rs

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,12 @@ use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::net::TcpListener;
-use std::time::Duration;
-
-use clokwerk::{AsyncScheduler, TimeUnits};
 
 use rss_aggregator::database::init_database;
 use rss_aggregator::model::configuration::ApplicationConfiguration;
 use rss_aggregator::observability::{get_subscriber, init_subscriber};
+use rss_aggregator::poller::start_poller;
 use rss_aggregator::startup;
-use rss_aggregator::services::channels::ChannelService;
-use rss_aggregator::services::items::ItemService;
-use rss_aggregator::services::GlobalService;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -25,41 +20,14 @@ async fn main() -> std::io::Result<()> {
 
     let db = init_database().await;
 
-    let item_service = ItemService::new(db.clone());
-    let channel_service = ChannelService::new(db.clone());
-    let global_service = GlobalService::new(item_service.clone(), channel_service.clone());
-
     let configuration = load_configuration().unwrap();
-
-    let mut scheduler = AsyncScheduler::new();
-    let global = global_service.clone();
-
-    let polling = std::env::var("POLLING_INTERVAL")
-        .unwrap_or_else(|_| String::from("300"))
-        .parse::<u32>()
-        .unwrap()
-        .seconds();
-    tracing::info!("Poll every {:?}", polling);
-
     let listener = TcpListener::bind(
         env::var("RSS_AGGREGATOR_LISTEN_ON").unwrap_or_else(|_| String::from("0.0.0.0:8080")),
     )?;
 
-    scheduler
-        .every(polling)
-        .run(move || refresh(global.clone()));
-    actix_rt::spawn(async move {
-        loop {
-            scheduler.run_pending().await;
-            actix_rt::time::sleep(Duration::from_millis(100)).await;
-        }
-    });
+    start_poller(db.clone()).await;
 
     startup::startup(db, configuration, listener).await
-}
-
-async fn refresh(service: GlobalService) {
-    service.refresh_all_channels().await;
 }
 
 fn load_configuration() -> Result<ApplicationConfiguration, Box<dyn Error>> {
