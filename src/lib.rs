1b9a1ba29dc7a3ae7bc3aefc7d2c84e6bfc02fab
Eric <eric@pedr0.net>
2022-08-20 18:22:06 +0200

    Create a common crate with observability stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod databases;
 pub mod errors;
 pub mod model;
-pub mod observability;
 pub mod routes;
 pub mod services;
 pub mod startup;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
+use rss_aggregator::databases::{init_postgres_connection, init_redis_connection};
+use rss_aggregator::startup;
 use std::env;
 use std::net::TcpListener;
-
-use rss_aggregator::databases::{init_postgres_connection, init_redis_connection};
-use rss_aggregator::observability;
-use rss_aggregator::observability::{get_subscriber, init_subscriber};
-use rss_aggregator::startup;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Init dotenv
     dotenv::dotenv().ok();
 
-    let subscriber = get_subscriber("rss_aggregator".into(), "info".into());
-    init_subscriber(subscriber);
+    let subscriber =
+        rss_common::observability::get_subscriber("rss_aggregator".into(), "info".into());
+    rss_common::observability::init_subscriber(subscriber);
 
     let postgres_connection = init_postgres_connection().await;
     let redis_pool = init_redis_connection();
@@ -21,7 +19,7 @@ async fn main() -> std::io::Result<()> {
         env::var("RSS_AGGREGATOR_LISTEN_ON").unwrap_or_else(|_| String::from("0.0.0.0:8080")),
     )?;
 
-    let _sentry_guard = observability::init_sentry();
+    let _sentry_guard = rss_common::observability::init_sentry();
 
     startup::startup(postgres_connection, redis_pool, listener).await
 }
