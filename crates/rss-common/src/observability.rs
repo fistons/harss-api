aec6246f47f79f2b7b50fe87e27d4826539e9c06
Eric <emercier@weborama.com>
2023-01-16 08:29:56 +0100

    Add datadog APM

--- crates/rss-common/src/observability.rs
+++ crates/rss-common/src/observability.rs
@@ -1,19 +1,20 @@
+use opentelemetry::sdk::trace::Tracer;
 use sentry::ClientInitGuard;
 use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_log::LogTracer;
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 pub fn get_subscriber(name: &str, env_filter: &str) -> impl Subscriber + Sync + Send {
     // Building the jaeger layer, if needed
-    let telemetry = if std::env::var("JAEGER_ENABLED").is_ok() {
-        let tracer = opentelemetry_jaeger::new_agent_pipeline()
-            .with_service_name(name)
-            .install_batch(opentelemetry::runtime::Tokio)
-            .unwrap();
-        Some(tracing_opentelemetry::layer().with_tracer(tracer))
-    } else {
-        None
-    };
+    let jaeger = build_jaeger(name);
+
+    let datadog = opentelemetry_datadog::new_pipeline()
+        .with_service_name(name)
+        .install_batch(opentelemetry::runtime::Tokio)
+        .ok()
+        .map(|x| tracing_opentelemetry::layer().with_tracer(x));
 
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
@@ -23,7 +24,8 @@ pub fn get_subscriber(name: &str, env_filter: &str) -> impl Subscriber + Sync + 
         .with(env_filter)
         .with(fmt)
         .with(sentry_tracing::layer())
-        .with(telemetry)
+        .with(jaeger)
+        .with(datadog)
 }
 pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
     LogTracer::init().expect("Failed to set logger");
@@ -35,4 +37,20 @@ pub fn init_sentry() -> ClientInitGuard {
         release: sentry::release_name!(),
         ..Default::default()
     })
+}
+
+fn build_jaeger<S>(name: &str) -> Option<OpenTelemetryLayer<S, Tracer>>
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+{
+    // Building the jaeger layer, if needed
+    if std::env::var("JAEGER_ENABLED").is_ok() {
+        let tracer = opentelemetry_jaeger::new_agent_pipeline()
+            .with_service_name(name)
+            .install_batch(opentelemetry::runtime::Tokio)
+            .unwrap();
+        Some(tracing_opentelemetry::layer().with_tracer(tracer))
+    } else {
+        None
+    }
 }
