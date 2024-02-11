e53fdd4923d931d874bb3a7f18ad869e44cf5f19
Eric <eric@pedr0.net>
2024-02-11 10:24:24 +0100

    Try to improve tracing a bit. Still lot to do on this.
    Thanks @jonhoo

--- src/common/observability.rs
+++ src/common/observability.rs
@@ -8,14 +8,13 @@ use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 pub fn get_subscriber(name: &str, env_filter: &str) -> impl Subscriber + Sync + Send {
-    let telemetry = build_datadog(name).or_else(|| build_jaeger(name));
-
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
     let fmt = tracing_subscriber::fmt::Layer::default();
 
     Registry::default()
-        .with(telemetry)
+        .with(build_datadog(name))
+        .with(build_jaeger(name))
         .with(env_filter)
         .with(fmt)
 }
@@ -30,11 +29,11 @@ where
     S: Subscriber + for<'a> LookupSpan<'a>,
 {
     // Building the jaeger layer, if needed
-    if var("JAEGER_ENABLED").is_ok() {
+    if var("JAEGER_AGENT_ENDPOINT").is_ok() {
         opentelemetry_jaeger::new_agent_pipeline()
             .with_service_name(name)
             .install_batch(opentelemetry::runtime::Tokio)
-            .map_err(|err| eprintln!("Datadog error {:?}", err))
+            .map_err(|err| eprintln!("Jaeger error {:?}", err))
             .ok()
             .map(|x| tracing_opentelemetry::layer().with_tracer(x))
     } else {
@@ -46,12 +45,10 @@ fn build_datadog<S>(name: &str) -> Option<OpenTelemetryLayer<S, Tracer>>
 where
     S: Subscriber + for<'a> LookupSpan<'a>,
 {
-    if var("DD_ENABLED").is_ok() {
+    if let Ok(dd_agent) = var("DD_AGENT_ENDPOINT") {
         opentelemetry_datadog::new_pipeline()
             .with_service_name(name)
-            .with_agent_endpoint(
-                var("DD_AGENT").unwrap_or_else(|_| "http://127.0.0.1:8126".to_owned()),
-            )
+            .with_agent_endpoint(dd_agent)
             .with_trace_config(
                 trace::config()
                     .with_sampler(Sampler::AlwaysOn) //TODO Add sampling, one day.
